367d831687039e2c10f2c605f0352d04c09a8e95
mayurck23 <mayurchavan306@gmail.com>
2025-02-19 10:21:45 +0530

    git testing

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -83,6 +83,7 @@ export class CustomerComponent {
        // console.log(this.UserData.values);
         this.dataSource.data = [...this.UserData]; 
         console.log('Data added:', this.UserData);
+        console.log('test')
 
       }
 
